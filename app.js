ada714e76490ba4f278a2cc6f015bfaf4783ddc2
SuNg Do <sungdo1004@gmail.com>
2020-10-13 14:04:33 -0800

    mongo hooked up

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// app
+const app = express();
+
+// db
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}).then(() => console.log('DB connected'))
+
+
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('hello from node');
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
