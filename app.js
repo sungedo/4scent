ae15540d89ed2f2f41e0198047a5b3ea77eb544d
SuNg Do <sungdo1004@gmail.com>
2020-10-16 10:05:47 -0800

    user by id

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 require('dotenv').config();
 
+const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 // app
 const app = express();
@@ -29,6 +30,7 @@ app.use(expressValidator());
 
 // routes middleware
 
+app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8001;
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, signin } = require('../controllers/user');
-const { userSignupValidator } = require('../validator')
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 
-router.post('/signup', userSignupValidator, signup);
-router.post('/signin', signin);
+router.get('/secret/:userId', requireSignin, (req, res) => {
+    res.json({
+        user: req.profile
+    })
+});
+
+
+router.param('userId', userById)
 
 
 
