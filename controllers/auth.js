f263ca5a829bf459b1554bb5af08c74769c89521
SuNg Do <sungdo1004@gmail.com>
2020-10-16 11:05:14 -0800

    isAdmin auth added

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,3 +58,23 @@ exports.requireSignin = expressJwt({
     algorithms: ["HS256"], // added later
     userProperty: "auth",
   });
+
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id
+        if(!user) {
+            return res.status(402).json({
+                error : "Acess Denied"
+            });
+        }
+    next();
+}
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'Admin resouce!  Acess Denied'
+        })
+    };
+    next();
+}
