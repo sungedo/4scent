726e389bc8767830a36fcb2ac12a43f11cf61577
SuNg Do <sungdo1004@gmail.com>
2020-10-17 13:51:11 -0800

    product update

--- controllers/product.js
+++ controllers/product.js
@@ -60,3 +60,57 @@ exports.create = (req, res) => {
 
     })
 };
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({
+            message: "Product deleted successfully"
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+
+        const {name, description, price, category, quantity, shipping} = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });    
+        }
+
+
+        let product = req.product
+        product = _.extend(product, fields)
+
+        if(files.photo) {
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        } 
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error : errorHandler(err)
+                })
+            }
+            res.json(result)
+
+        })
+
+    })
+};
