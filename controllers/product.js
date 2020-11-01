53575664a3dadd2fb733955730b170086e66dd4c
SuNg Do <sungdo1004@gmail.com>
2020-10-31 16:24:34 -0800

    list categories done

--- controllers/product.js
+++ controllers/product.js
@@ -114,3 +114,57 @@ exports.update = (req, res) => {
 
     })
 };
+
+// by sell = /products?sortBy=sold&order=desc&limit=4
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6
+
+    Product.find()
+        .select("-photo")
+        .populate('category')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, product) => {
+            if(err) {
+                return res.status(400).json({
+                    error : 'Product not found'
+                })
+            }
+            res.json(product)
+
+        })
+}
+
+
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find({_id: {$ne: req.product}, category: req.product.category})
+    .limit(limit)
+    .populate('category', '_id name')
+    .exec((err, products) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Products not found'
+            });
+        }
+        res.json(products)
+
+    })
+    
+}
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, categories) => {
+        if(err) {
+            return res.status(400).json({
+                error : "Categories not found"
+            })
+        }
+        res.json(categories)
+
+    }) 
+}
